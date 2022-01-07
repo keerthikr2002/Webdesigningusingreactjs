87f6040a7b6ec911a6ce6b5515507a723239b0dd
suryanarla <suryanarla54121@gmail.com>
2022-01-07 16:25:24 +0530

    added js

--- project/index.js
+++ project/index.js
@@ -12,4 +12,23 @@ function loadjson(file, callback) {
 loadjson("data.json", function (text) {
   var data = JSON.parse(text);
   console.log(data);
+  Display(data.cards);
 });
+
+//  fun
+function Display(mb) {
+  var deck = document.querySelector(".card-deck");
+
+  for (var i in mb) {
+    // console.log(mb[i].name);
+    var cad = document.createElement("div");
+    cad.classList.add("card");
+    deck.append(cad);
+    var im = document.createElement("img");
+    im.src = mb[i].img;
+    cad.append(im);
+    var nam = document.createElement("h1");
+    nam.textContent = mb[i].name;
+    cad.append(nam);
+  }
+}
